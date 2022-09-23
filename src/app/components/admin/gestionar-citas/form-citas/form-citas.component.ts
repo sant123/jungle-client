b141548e43663305edaf50f72ac79e1b2ed7807b
Santiago Aguilar Hernández <santiago.aguilar.821@gmail.com>
2022-09-23 01:55:45 -0500

    Se agrega el módulo para crear citas (#8)

--- src/app/components/admin/gestionar-citas/form-citas/form-citas.component.ts
+++ src/app/components/admin/gestionar-citas/form-citas/form-citas.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup,FormBuilder,Validators, ControlContainer } from '@angular/forms';
+import {
+  FormControl,
+  FormGroup,
+  FormBuilder,
+  Validators,
+  ControlContainer,
+} from '@angular/forms';
 import { CitaService } from '../../../../services/cita.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import { DetalleCitaService } from '../../../../services/detalleCita.service';
@@ -8,123 +14,179 @@ import Swal from 'sweetalert2';
 @Component({
   selector: 'app-form-citas',
   templateUrl: './form-citas.component.html',
-  styleUrls: ['./form-citas.component.css']
+  styleUrls: ['./form-citas.component.css'],
 })
 export class FormCitasComponent implements OnInit {
   formularioCitas: FormGroup = this.createFormGroup();
-  detalleCita:any=null;
-  idCitaActualizar:any = null;
-  horas:any=[];
+  detalleCita: any = null;
+  idCitaActualizar: any = null;
+  horas: string[] = [];
+  sedes: {
+    nombre: string;
+    direccion: string;
+  }[] = [];
+  barberos: {
+    id: number;
+    nombre: string;
+    apellido: string;
+  }[] = [];
+  clientes: {
+    id: number;
+    nombre: string;
+    apellido: string;
+  }[] = [];
 
-
-  constructor(private citaService:CitaService,private detalleCitaService:DetalleCitaService,
-    private _route:ActivatedRoute ,private  router: Router) { }
+  constructor(
+    private citaService: CitaService,
+    private detalleCitaService: DetalleCitaService,
+    private _route: ActivatedRoute,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
-          // obtiene los datos del servicio que va traer lo que voy a mostrar en el select
+    // obtiene los datos del servicio que va traer lo que voy a mostrar en el select
 
-          // this.detalleCitaService.getDetalleCita().subscribe((respuesta)=>{
-          //   this.detalleCita=respuesta;
+    // this.detalleCitaService.getDetalleCita().subscribe((respuesta)=>{
+    //   this.detalleCita=respuesta;
 
+    //   this.idCitaActualizar = this._route.snapshot.paramMap.get('id');
 
-          //   this.idCitaActualizar = this._route.snapshot.paramMap.get('id');
+    // })
 
+    // this.idCitaActualizar = this._route.snapshot.paramMap.get('id');
 
-          // })
+    // if (this.idCitaActualizar !== null) {
+    //   this.citaService
+    //     .getCitaById(this.idCitaActualizar)
+    //     .subscribe((cita: any) => {
+    //       this.formularioCitas.get('Fecha')?.setValue(cita.fecha);
+    //       this.formularioCitas.get('Hora')?.setValue(cita.hora);
+    //     });
+    // }
 
-          this.idCitaActualizar = this._route.snapshot.paramMap.get('id');
-          if(this.idCitaActualizar!==null){
-              this.citaService.getCitaById(this.idCitaActualizar).subscribe((cita:any)=>{
-                this.formularioCitas.get('Fecha')?.setValue(cita.fecha);
-                this.formularioCitas.get('Hora')?.setValue(cita.hora);
-
-
-              });
-
-        }
-
-
-        this.horas=['09:00-09:30','10:00-10:30','11:00-11:30','12:00-12:30','13:00-13:30'
-        ,'14:00-14:30','15:00-15:30','16:00-16:30','17:00-17:30','18:00-18:30','19:00-19:30'
-       ,'20:00-20:30','21:00-21:30']
-  }
-
-  get FechaNovalido(){
-    return this.formularioCitas.get('Fecha')?.invalid && this.formularioCitas.get('Fecha')?.touched
-     }
-
-     get HoraNovalido(){
-      return this.formularioCitas.get('Hora')?.invalid && this.formularioCitas.get('Hora')?.touched
-       }
-
-
-     
-
-
-
-
-  createFormGroup(){
-    return new FormGroup({
-      Fecha: new FormControl("",[Validators.required ]),
-      Hora: new FormControl("",[Validators.required ]),
-      IdUsuarioAtiende: new FormControl("",[Validators.required ]),
-
-
-
+    this.citaService.getBarberos().subscribe((barberos: any) => {
+      this.barberos = barberos;
     });
 
+    this.citaService.getClientes().subscribe((clientes: any) => {
+      this.clientes = clientes;
+    });
+
+    this.horas = [
+      '09:00-09:30',
+      '10:00-10:30',
+      '11:00-11:30',
+      '12:00-12:30',
+      '13:00-13:30',
+      '14:00-14:30',
+      '15:00-15:30',
+      '16:00-16:30',
+      '17:00-17:30',
+      '18:00-18:30',
+      '19:00-19:30',
+      '20:00-20:30',
+      '21:00-21:30',
+    ];
+
+    this.sedes = [
+      {
+        nombre: 'Sede 1',
+        direccion: 'Diagonal 47A Sur # 53A - 22, Barrio Venecia',
+      },
+      {
+        nombre: 'Sede 2',
+        direccion: 'Diagonal 49A Sur # 53A - 22, Barrio Venecia',
+      },
+    ];
   }
 
-  registrarCita(){
-    Swal.fire({
+  get HoraNovalido() {
+    return (
+      this.formularioCitas.get('Hora')?.invalid &&
+      this.formularioCitas.get('Hora')?.touched
+    );
+  }
+
+  get FechaNovalido() {
+    return (
+      this.formularioCitas.get('Fecha')?.invalid &&
+      this.formularioCitas.get('Fecha')?.touched
+    );
+  }
+
+  get SedeNovalido() {
+    return (
+      this.formularioCitas.get('Sede')?.invalid &&
+      this.formularioCitas.get('Sede')?.touched
+    );
+  }
+
+  get IdUsuarioAgendaNovalido() {
+    return (
+      this.formularioCitas.get('IdUsuarioAgenda')?.invalid &&
+      this.formularioCitas.get('IdUsuarioAgenda')?.touched
+    );
+  }
+
+  get IdUsuarioAtiendeNovalido() {
+    return (
+      this.formularioCitas.get('IdUsuarioAtiende')?.invalid &&
+      this.formularioCitas.get('IdUsuarioAtiende')?.touched
+    );
+  }
+
+  createFormGroup() {
+    return new FormGroup({
+      Hora: new FormControl('', [Validators.required]),
+      Fecha: new FormControl('', [Validators.required]),
+      Sede: new FormControl('', [Validators.required]),
+      IdUsuarioAgenda: new FormControl('', [Validators.required]),
+      IdUsuarioAtiende: new FormControl('', [Validators.required]),
+    });
+  }
+
+  async registrarCita() {
+    const result = await Swal.fire({
       title: 'Esta seguro que desea registrar este Servicio',
       showDenyButton: true,
       showCancelButton: true,
       confirmButtonText: 'registrar',
-    }).then((result) => {
-  if (result.isConfirmed) {
+    });
+
+    if (!result.isConfirmed) {
+      return;
+    }
+
     const formData = new FormData();
-    formData.append("Fecha", this.formularioCitas.value.Fecha);
-    formData.append("Hora", this.formularioCitas.value.Hora);
-    formData.append("IdUsuarioAtiende", this.formularioCitas.value.IdUsuarioAtiende);
 
+    const [HoraInicio, HoraFin] = this.formularioCitas.value.Hora.split('-');
 
+    formData.append('HoraInicio', HoraInicio);
+    formData.append('HoraFin', HoraFin);
+    formData.append('Fecha', this.formularioCitas.value.Fecha);
+    formData.append('Sede', this.formularioCitas.value.Sede);
+    formData.append('IdUsuarioAgenda', this.formularioCitas.value.IdUsuarioAgenda);
+    formData.append('IdUsuarioAtiende', this.formularioCitas.value.IdUsuarioAtiende);
 
+    this.citaService.insertarCita(formData).subscribe((respuesta) => {
+      Swal.fire('Se ha registrado', '', 'success').then((result) => {
+        //redirecciona al componente de gestionar Productos
+        this.router.navigate(['admin/gestionarCitas']);
+        this.formularioCitas.reset();
+      });
+    });
 
-    if(this.idCitaActualizar!==null){
-      formData.append("Id", this.idCitaActualizar);
-      this.citaService.ActualizarCita(formData).subscribe((resultado)=>{
+    // if (this.idCitaActualizar !== null) {
+    //   formData.append('Id', this.idCitaActualizar);
+    //   this.citaService.ActualizarCita(formData).subscribe((resultado) => {
+    //     Swal.fire('Se ha Actualizado', '', 'success').then((result) => {
+    //       //redirecciona al componente de gestionar Productos
+    //       this.router.navigate(['admin/gestionarCitas']);
+    //       this.formularioCitas.reset();
+    //     });
+    //   });
+    // } else {
 
-        Swal.fire('Se ha Actualizado', '', 'success').then(result=>{
-          //redirecciona al componente de gestionar Productos
-         this.router.navigate(['admin/gestionarCitas']);
-         this.formularioCitas.reset();
-       })
-
-     })
-    }
-    else{
-      this.citaService.insertarCita(formData).subscribe((respuesta)=>{
-
-        Swal.fire('Se ha registrado', '', 'success').then(result=>{
-          //redirecciona al componente de gestionar Productos
-         this.router.navigate(['admin/gestionarCitas']);
-         this.formularioCitas.reset();
-       })
-
-     })
-    }
-
+    // }
   }
-
-  })
-
-
-
-
-
-
-
-
-}
 }
--- src/app/services/cita.service.ts
+++ src/app/services/cita.service.ts
@@ -1,33 +1,36 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from "@angular/common/http";
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CitaService {
-
-  readonly BaseUrl = "https://localhost:44323/api/cita";
-  constructor(private http: HttpClient) { }
-
+  readonly BaseUrl = 'https://localhost:44323/api/cita';
+  constructor(private http: HttpClient) {}
 
   getCita() {
     //https://localhost:44324/api/persona -- nombre del controlador que quiero solicitar
-        return this.http.get(this.BaseUrl);
+    return this.http.get(this.BaseUrl);
+  }
 
-      }
+  insertarCita(formGroup: any) {
+    return this.http.post(this.BaseUrl, formGroup);
+  }
 
-      insertarCita(formGroup:any) {
-        return this.http.post(this.BaseUrl, formGroup);
-      }
+  getCitaById(id: any) {
+    //https://localhost:44324/api/persona -- nombre del controlador que quiero solicitar
+    return this.http.get(this.BaseUrl + '/' + id);
+  }
 
-      getCitaById(id:any) {
-        //https://localhost:44324/api/persona -- nombre del controlador que quiero solicitar
-            return this.http.get(this.BaseUrl+'/'+id);
+  ActualizarCita(formGroup: any) {
+    return this.http.put(this.BaseUrl, formGroup);
+  }
 
-          }
+  getBarberos() {
+    return this.http.get(this.BaseUrl + '/Barberos');
+  }
 
-          ActualizarCita(formGroup:any) {
-            return this.http.put(this.BaseUrl, formGroup);
-          }
-
+  getClientes() {
+    return this.http.get(this.BaseUrl + '/Clientes');
+  }
 }
